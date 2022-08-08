739a3004ff3001f476f801c7914127dbad85dacc
yeonghoonPark <95198410+yeonghoonPark@users.noreply.github.com>
2022-08-08 19:04:46 +0900

    Add files via upload

---
+++ src/components/ButtonDefault.vue
@@ -0,0 +1,35 @@
+<template>
+  <button>{{ message }}</button>
+</template>
+
+//
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: "default",
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  all: unset;
+  width: 120px;
+  height: 50px;
+  margin: 1rem auto;
+  border-radius: 5px;
+  text-align: center;
+  background: #111101;
+  color: #ffffff;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+button:hover {
+  color: #3dc074;
+  transform: scale(1.1);
+}
+</style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -4,34 +4,97 @@
       <table>
         <thead>
           <tr>
-            <th>상영영화</th>
-            <th>한줄평</th>
-            <th>감독에게</th>
-            <th>평점</th>
-            <th>포스터</th>
+            <th>{{ theadTitle.th1 }}</th>
+            <th>{{ theadTitle.th2 }}</th>
+            <th>{{ theadTitle.th3 }}</th>
+            <th>{{ theadTitle.th4 }}</th>
+            <th>{{ theadTitle.th5 }}</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(movie, index) in movieList" :key="index" @click="changeBackgroundColor">
+          <tr
+            v-for="(movie, index) in movieList"
+            :key="index"
+            @click="changeBackgroundColor"
+            @mouseup="showPoster"
+          >
             <td>{{ movie.movieName }}</td>
             <td><input type="text" /></td>
             <td><input type="text" /></td>
-            <td><input type="number" max="10" min="0" @click="onChangeTest" /></td>
-            <td class="hidden">
+            <td>
+              <input type="text" max="5" min="0" title="Max Score is 5" />
+            </td>
+            <td class="hidden" @click.capture="showImage">
               <img :src="movie.imageSrc" :alt="movie.movieName" />
+              <div class="modal-image" v-if="isIfshow">
+                <img :src="movie.imageSrc" :alt="movie.movieName" />
+                <div class="btn-box">
+                  <ButtonModalClose
+                    style="margin-top: 9rem"
+                    :message="close"
+                    @click="modalClosing"
+                  />
+                </div>
+              </div>
             </td>
           </tr>
         </tbody>
       </table>
-      <button type="button">확인</button>
+      <form>
+        <div class="btn-box">
+          <ButtonTable :message="confirm" @click="onSubmit" />
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+// import { provide } from "@vue/runtime-core";
+// import { ref } from "vue";
+// import { reactive } from "vue";
+import ButtonTable from "./ButtonDefault.vue";
+import ButtonModalClose from "./ButtonDefault.vue";
+
 export default {
-  components: {},
+  components: { ButtonTable, ButtonModalClose },
+  // name의 목적?
   name: "Table-",
+  setup() {
+    // provide("message", "닫기");
+
+    const CLASS_NAME_ACTIVE = "active";
+    const CLASS_NAME_HIDDEN = "hidden";
+
+    const classNameActiveRemove = (trElement) => {
+      trElement.forEach((trElement) => {
+        trElement.classList.remove(CLASS_NAME_ACTIVE);
+      });
+    };
+
+    const changeBackgroundColor = (event) => {
+      const trElement = document.querySelectorAll("tbody>tr");
+      const target = event.currentTarget;
+      classNameActiveRemove(trElement);
+      target.classList.add(CLASS_NAME_ACTIVE);
+    };
+
+    const classNameAddHidden = (lastTdElements) => {
+      lastTdElements.forEach((lastTdElement) => {
+        lastTdElement.classList.add(CLASS_NAME_HIDDEN);
+      });
+    };
+
+    const showPoster = (event) => {
+      const lastTdElement = document.querySelectorAll("tbody>tr>td:last-child");
+      const target = event.currentTarget;
+      const lastTarget = target.lastChild;
+      classNameAddHidden(lastTdElement);
+      lastTarget.classList.remove(CLASS_NAME_HIDDEN);
+    };
+
+    return { changeBackgroundColor, showPoster };
+  },
   data() {
     return {
       movieList: [
@@ -71,30 +134,57 @@ export default {
           imageSrc: require("../assets/symantec_error_the_movie.jpg"),
         },
       ],
+      theadTitle: {
+        th1: "영화",
+        th2: "소감",
+        th3: "평가",
+        th4: "평점",
+        th5: "포스터",
+      },
+      close: "닫기",
+      confirm: "확인",
+      isIfshow: false,
+
+      testValue: "",
+      testValue2: "",
+      as: "",
     };
   },
   methods: {
-    changeBackgroundColor(event) {
-      const trElement = document.querySelectorAll("tr");
-      const target = event.currentTarget;
-      this.classNameActiveRemove(trElement);
-      target.classList.add("active");
-      this.testFunction(event);
+    showImage() {
+      this.isIfshow = true;
     },
-    classNameActiveRemove(target) {
-      target.forEach((trElement) => {
-        trElement.classList.remove("active");
+
+    modalClosing(event) {
+      event.stopPropagation();
+      this.isIfshow = false;
+    },
+
+    onSubmit(event) {
+      event.preventDefault();
+      const inputs = document.querySelectorAll("input");
+      console.log(inputs[0]);
+      inputs.forEach((input) => {
+        if (input.value !== "") {
+          console.log("완료");
+        } else {
+          console.log("빈칸없이 입력해주세요");
+        }
       });
-    },
-    testFunction(event) {
-      console.log(event.currentTarget.lastChild);
-      event.currentTarget.lastChild.classList.remove("hidden");
     },
   },
 };
 </script>
 
 <style scoped>
+.hidden {
+  display: none;
+}
+
+.show {
+  display: show;
+}
+
 .table {
   width: 100%;
   margin: 77px auto 0;
@@ -106,7 +196,7 @@ export default {
 
 table {
   width: 100%;
-  border: 1px solid #000;
+  border: 1px solid #111101;
   text-align: center;
 }
 
@@ -141,20 +231,24 @@ td img {
   cursor: zoom-in;
 }
 
-button {
-  /* all: unset; */
+.btn-box {
   width: 100%;
-  padding: 1rem;
-  border: 1px solid #000;
-  border-top: none;
   text-align: center;
 }
 
-.hidden {
-  display: none;
+.modal-image {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+
+  background: rgba(0, 0, 0, 0.2);
 }
 
-.show {
-  display: show;
+.modal-image img {
+  width: 440px;
+  height: 620px;
+  transform: translateY(20%);
 }
 </style>
